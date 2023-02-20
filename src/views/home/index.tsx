0bd51e8167c09aba9294ae8627714651ea2beaef
Owen <owenventer99@gmail.com>
2023-02-20 22:37:45 +0200

    updated some text on the login page

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -38,12 +38,12 @@ export const HomeView: FC = ({ }) => {
         <div className='mt-6'>
         <div className='text-sm font-normal align-bottom text-right text-slate-600 mt-4'>v{pkg.version}</div>
         <h1 className="text-center text-5xl md:pl-12 font-bold text-transparent bg-clip-text bg-gradient-to-br from-indigo-500 to-fuchsia-500 mb-4">
-          Solana Next
+         PixelBands Education
         </h1>
         </div>
         <h4 className="md:w-full text-2x1 md:text-4xl text-center text-slate-300 my-2">
-          <p>Unleash the full power of blockchain with Solana and Next.js 13.</p>
-          <p className='text-slate-500 text-2x1 leading-relaxed'>Full-stack Solana applications made easy.</p>
+          <p>Sign in to access the learning material.</p>
+          <p className='text-slate-500 text-2x1 leading-relaxed'>Only holders of PixelBands NFTs will be granted access</p>
         </h4>
         
         <div className="flex flex-col mt-2">
